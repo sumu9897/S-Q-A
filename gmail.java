c2fd8ff074e1e08ddddb44a4c62a444a43a802e9
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-07-16 22:44:32 +0600

    Create gmail.java
    Open Gmail by SQA

---
+++ gmail.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Main {
+    WebDriver driver;
+
+    public void launchBrowser(){
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/mohammadsumon/Downloads/chromedriver_mac_arm64 (1)/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.gmail.com");
+    }
+    public static void main(String[] args) {
+
+        Main obj=new Main();
+        obj.launchBrowser();
+    }
+}
